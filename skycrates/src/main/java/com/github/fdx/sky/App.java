4a9e98eb53c5364843883c89a3d2a9b1f1a2b81b
Kyle Guarco <kyleguarco55@gmail.com>
2018-08-29 19:52:59 -0400

    Added spigotmc dependencies from local m2

--- skycrates/src/main/java/com/github/fdx/sky/App.java
+++ skycrates/src/main/java/com/github/fdx/sky/App.java
@@ -1,3 +1,14 @@
 package com.github.fdx.sky;
 
-public class App {}
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class App extends JavaPlugin 
+{
+    public App() {}
+
+    @Override
+    public void onEnable() {}
+
+    @Override
+    public void onDisable() {}
+}
