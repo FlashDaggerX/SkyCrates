ca903e15620d7148c1b58cea7ce5f4106cf30211
Kyle Guarco <kyleguarco55@gmail.com>
2019-11-27 09:34:05 -0500

    Created the 2019 archive

--- src-archive/2019/com/github/flashdaggers/sky/SkyCrates.java
+++ src-archive/2019/com/github/flashdaggers/sky/SkyCrates.java
@@ -2,6 +2,12 @@ package com.github.fdx.sky;
 
 import java.io.File;
 
+import com.github.fdx.sky.generator.DeliveryQueue;
+import com.github.fdx.sky.treasure.Crate;
+import com.github.fdx.sky.treasure.Treasure;
+import com.github.fdx.sky.value.AValue.Rarity;
+
+import org.bukkit.Material;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.plugin.java.JavaPlugin;
 import org.bukkit.scheduler.BukkitTask;
@@ -22,6 +28,17 @@ public class SkyCrates extends JavaPlugin {
     public void onEnable() {
         SkyCrates.INST = this;
         SkyCrates.DATA = getDataFolder();
+
+        if (!SkyCrates.DATA.exists()) SkyCrates.DATA.mkdir();
+
+        DeliveryQueue.push(new Crate(Rarity.NORMAL));
+        DeliveryQueue.peek().populate(new Treasure[] { 
+            new Treasure(Material.ACACIA_DOOR, Rarity.FIXED),
+            new Treasure(Material.DIAMOND_AXE, Rarity.GREAT, 1, 50)
+        });
+
+        this.scheduler = new DeliveryQueue().runTaskTimer(
+            SkyCrates.INST, 0, DeliveryQueue.PERIOD);
     }
 
     @Override
